14f5dd4e45ebe80509ba58d5f0f8d95b0437eed3
TahaSekk <sohailtaha128@gmail.com>
2022-07-25 00:59:18 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -4,9 +4,30 @@ let calculate=()=>{
     let chemistry=document.querySelector('#Chemistry').value;
     let biology=document.querySelector('#Biology').value;
   let result =document.querySelector('p');
-  let totalMakrs= parseFloat(maths) + parseFloat(physics)+parseFloat(chemistry)+parseFloat(biology)
-// console.log(biology);
-alert(totalMakrs)
+  let totalMakrs= parseFloat(maths)+ parseFloat(physics)+parseFloat(chemistry)+parseFloat(biology)
+ let grades="";
+let per=(totalMakrs*100)/400
+  // console.log(biology);
 // console.log(result.innerHTML='we here')
+// result.innerHTML=(totalMakrs);
+
+
+try {
+    if(per>=80 && per<100){
+      grades='A you are pass'
+    }
+    else if (per>=60 && per<79){
+      grades='B you are pass'
+    }
+    else if (per>=40 && per<59){
+      grades='C you are pass'
+    }
+    else{
+      grades='You are fail'
+    }
+
+} catch (error) {
+    alert(error.message)
+}
 
 }
